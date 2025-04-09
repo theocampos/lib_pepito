ef7cf73268751899ce7bfcafbd4aa26ef1f4082a
Exelo <exelo.corp@gmail.com>
2025-04-09 12:30:55 +0200

    feat: add init steam turbine header

---
+++ includes/pepito.h
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2024
+** Pepito library
+** File description:
+** pepito.h
+*/
+
+
+#ifndef INCLUDES_PEPITO_H_
+    #define INCLUDES_PEPITO_H_
+
+void init_steam_turbine();
+
+#endif /* INCLUDES_PEPITO_H_ */
